ca4d0d15071f2438faa2b1749b52bb9dcf6ecbff
João Eduardo <joao.eduardo.souza@hotmail.com>
2024-05-17 16:40:29 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'; 
+import ListaDeTarefas from './components/Lista'; 
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="App"> 
+    <ListaDeTarefas/>
+    </div>
+    </>
+  )
+}
+
+export default App
